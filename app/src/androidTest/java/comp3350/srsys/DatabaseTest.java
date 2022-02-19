7ffba7aa19df5c00ff323049f6d14079793de30c
jaunger1999 <jaunger1999@gmail.com>
2022-02-19 00:04:12 -0600

    Started database interface
    -Added IDataface file
    -Wrote test for defined method

---
+++ app/src/androidTest/java/comp3350/srsys/DatabaseTest.java
@@ -0,0 +1,11 @@
+package comp3350.srsys;
+
+import org.junit.Test;
+
+public class DatabaseTest {
+
+    @Test
+    public void FetchRandomWord() {
+
+    }
+}
---
+++ app/src/main/java/comp3350/srsys/interfaces/IDatabase.java
@@ -0,0 +1,6 @@
+package comp3350.srsys.interfaces;
+
+public interface IDatabase {
+    void FetchRandomWord();
+
+}
