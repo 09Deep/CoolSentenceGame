4153082c83b4dd1adb9dbe0905bc29b536794b78
09Deep <93400017+09Deep@users.noreply.github.com>
2022-02-23 17:00:30 -0600

    Unit testing added for UI
    This tests gives error in setting up the rule for the unit testing.

---
+++ app/src/androidTest/java/com/coolsentencegame/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.coolsentencegame;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+public class MainActivityTest {
+
+    //@Rule
+    //public TestRule mainActivityTestRule = new ;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testActivityLaunch(){
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
