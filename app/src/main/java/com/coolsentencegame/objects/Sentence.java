9f776de73c6c53fe5ef4cdcc853701cac7ac95ec
jaunger1999 <jaunger1999@gmail.com>
2022-02-23 14:53:37 -0600

    Added a sentence class
    -stores an array of strings
    -compares player sentence to stored sentence

--- app/src/main/java/com/coolsentencegame/objects/Sentence.java
+++ app/src/main/java/com/coolsentencegame/objects/Sentence.java
@@ -1,11 +1,39 @@
 package com.coolsentencegame.objects;
 
-import java.util.ArrayList;
-
+/*
+ * Sentence
+ *
+ * Data structure that holds a sentence as an array of strings
+ * and compares it to a player input sentence to test if they
+ * remembered the sequence of words correctly.
+ */
 public class Sentence {
-    private ArrayList<String> words;
+    private String[] correctSentence;
+    private String[] playerSentence;
+
+    private int sentenceLength;
 
     public Sentence(String s) {
+        correctSentence = s.split(" ");
+        sentenceLength = correctSentence.length;
 
+        playerSentence = new String[sentenceLength];
+    }
+
+    /*
+     * isPlayerSentenceCorrect
+     *
+     * Returns a boolean that states whether a user input sentence is correct or not.
+     */
+    public boolean isPlayerSentenceCorrect() {
+        boolean correct = true; //only say there's a mistake when we find one.
+
+        //exit the for loop if correct is no longer true.
+        for(int i = 0; i < sentenceLength && correct; i++) {
+            //check if the words at i are the same
+            correct = correctSentence[i].compareTo(playerSentence[i]) == 0;
+        }
+
+        return correct;
     }
 }
--- app/src/main/java/com/coolsentencegame/objects/Word.java
+++
@@ -1,4 +0,0 @@
-package com.coolsentencegame.objects;
-
-public class Word {
-}
