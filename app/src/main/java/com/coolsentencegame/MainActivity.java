72b1ff887f0766755e416e3920088f5e251def4b
smigii <smigii@protonmail.com>
2022-02-22 20:18:18 -0600

    Undo changes to MainActivity
    I had changed them to load game_activity, hopefully changing it back
    will prevent any merge conflicts for Deep?

--- app/src/main/java/com/coolsentencegame/MainActivity.java
+++ app/src/main/java/com/coolsentencegame/MainActivity.java
@@ -2,7 +2,6 @@ package com.coolsentencegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, Game.class));
+        setContentView(R.layout.activity_main);
     }
 }
