3a5ee70ee86e8ae9ebe58939422a0963d2da0731
mehrnazsadrol <84254284+mehrnazsadrol@users.noreply.github.com>
2022-04-01 14:20:57 -0500

    ScorePersistenceTest

--- app/src/test/java/com/coolsentencegame/persistence/IScorePersistenceTest.java
+++ app/src/test/java/com/coolsentencegame/persistence/IScorePersistenceTest.java
@@ -12,11 +12,61 @@ import java.util.ArrayList;
 public class IScorePersistenceTest {
 
     @Test
-    public void test1()
-    {
-//        IScorePersistence scorePersistence = Services.getScorePersistence();
-//        ArrayList<Score> ar = scorePersistence.getPrevScores(1);
-//        assertEquals(1, ar.size());
+    public void test1() {
+
+
+        int size = 4;
+        ArrayList<Score> test_array = new ArrayList<>();
+
+        test_array.add(new Score(2, 3));
+        test_array.add(new Score(1, 3));
+        test_array.add(new Score(4, 7));
+        test_array.add(new Score(2, 9));
+
+        IScorePersistence s_Persistence = new MockScorePersistence();
+
+        s_Persistence.StoreScore(test_array.get(0));
+        s_Persistence.StoreScore(test_array.get(1));
+        s_Persistence.StoreScore(test_array.get(2));
+        s_Persistence.StoreScore(test_array.get(3));
+
+
+        ArrayList<Score> test_scores = s_Persistence.getPrevScores(size);
+        assertEquals(test_scores.size(), test_array.size());
+
+        for (int i = 0; i < size; i++) {
+            assert (test_scores.get(i).isEqual(test_array.get(i)));
+        }
+        s_Persistence.removeScore(test_array.get(0));
+        size--;
+        test_scores = s_Persistence.getPrevScores(size);
+        assertEquals(test_scores.size(), test_array.size());
+
+        for (int i = 0; i < size; i++) {
+            assert (test_scores.get(i).isEqual(test_array.get(i)));
+        }
+
+        s_Persistence.removeScore(test_array.get(0));
+        size--;
+        test_scores = s_Persistence.getPrevScores(size);
+        assertEquals(test_scores.size(), test_array.size());
+
+        for (int i = 0; i < size; i++) {
+            assert (test_scores.get(i).isEqual(test_array.get(i)));
+        }
+
+        test_array.add(new Score(8, 2));
+        test_array.add(new Score(9, 5));
+    size +=2;
+
+        test_scores = s_Persistence.getPrevScores(size);
+        assertEquals(test_scores.size(), test_array.size());
+
+        for (int i = 0; i < size; i++) {
+            assert (test_scores.get(i).isEqual(test_array.get(i)));
+        }
+
+
     }
 
 }
