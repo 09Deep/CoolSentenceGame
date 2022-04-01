a5696d2c3a2afe0742f2de9d96c69dfd339956cc
09Deep <93400017+09Deep@users.noreply.github.com>
2022-04-01 12:52:23 -0500

    level 2,3 removed

--- app/src/main/java/com/coolsentencegame/ui/GameLevelActivity.java
+++ app/src/main/java/com/coolsentencegame/ui/GameLevelActivity.java
@@ -34,23 +34,7 @@ public class GameLevelActivity extends AppCompatActivity {
             }
         });
 
-        Button buttonToLevel2 = findViewById(R.id.buttontolevel2);
-        buttonToLevel2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent toLevelTwo = new Intent(view.getContext(),LevelTwoActivity.class);
-                startActivity(toLevelTwo);
-            }
-        });
 
-        Button buttonToLevel3 = findViewById(R.id.buttontolevel3);
-        buttonToLevel3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent toLevelThree = new Intent(view.getContext(),LevelThreeActivity.class);
-                startActivity(toLevelThree);
-            }
-        });
 
     }
 }
